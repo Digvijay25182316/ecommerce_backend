26d5ba90da5e462ffd46a79f43d99f6f0903e4c1
Digvijay Edake <digvijays.edake>
2023-07-07 21:17:18 +0530

    something is resolved

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/myapp";
 const connectDB = () => {
   mongoose
-    .connect(MONGO_URI)
+    .connect(process.env.MONGO_URI)
     .then(({ connection }) =>
       console.log(`db is connected to ${connection.host}`)
     );
