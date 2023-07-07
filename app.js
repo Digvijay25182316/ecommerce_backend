47559fe62b6349045a2e11133ff227de32d5310b
Digvijay Edake <digvijays.edake>
2023-07-07 20:41:21 +0530

    another error fixed

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
     credentials: true,
   })
 );
