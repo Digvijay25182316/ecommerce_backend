25b0a55d249492a9eb7cfaf5e193d61b1ce14cf8
Digvijay Edake <digvijays.edake>
2023-07-11 13:18:45 +0530

    commit

--- app.js
+++ app.js
@@ -16,11 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
-    "Access-Control-Allow-Origin": [
-      process.env.FRONTEND_URL,
-      "http://localhost:3000",
-    ],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
