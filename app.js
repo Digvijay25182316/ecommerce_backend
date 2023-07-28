0613c1d2d4c03c236fd25950d457988037cf7952
Digvijay25182316 <digvijays.edake2002@gmail.com>
2023-07-28 11:48:58 +0530

    commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
