156eb0657c7e64e522eb9e8a3a94d5666f6838dc
Digvijay25182316 <digvijays.edake2002@gmail.com>
2024-01-18 11:29:58 +0530

    commit

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const app = require("./app");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const PORT = process.env.PORT || 8080;
-
+const PORT = 8080;
 app.listen(PORT, (req, res) => {
   console.log(`your server is running on http://localhost:${PORT}`);
 });
